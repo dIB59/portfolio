2431edd41862a9eb9b220ff07cd7d4b1e0dec24f
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-12-10 00:09:32 +0100

    updated fomatting

--- components/ground.tsx
+++ components/ground.tsx
@@ -1,10 +1,14 @@
-"use client"
+"use client";
 
 export function Ground() {
-  return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow>
-      <planeGeometry args={[200, 200]} />
-      <meshStandardMaterial color="#8fbc8f" flatShading />
-    </mesh>
-  )
+    return (
+        <mesh
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, 0, 0]}
+            receiveShadow
+        >
+            <planeGeometry args={[200, 200]} />
+            <meshStandardMaterial color="#8fbc8f" flatShading />
+        </mesh>
+    );
 }
